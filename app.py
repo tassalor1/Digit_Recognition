a48df3e3d456a9addfa729fbbd7e1d09a52882f0
Connor Walker <walkerconnor14@gmail.com>
2023-03-22 19:41:46 +0000

    Creating front end of the app

--- app.py
+++ app.py
@@ -19,5 +19,5 @@ def predict():
 
     return render_template('front.html')
 
-if __name__ == "--main__":
-    app.run(port=500, debug=True)
+if __name__  == "--main__":
+    app.run(port=5000, debug=True)
